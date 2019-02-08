1f54d4f344ed4f7037260d779f21cf1d721c4a2b
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-02-08 14:40:51 -0500

    First Commit

---
+++ UniversityClinicHospital/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityClinicHospital
+{
+    class Employee
+    {
+        public Employee()
+        {
+            string FirstName;
+            string LastName;
+            int Salary;
+            string JobTitle;
+            int EmployeeID;
+
+        }
+        
+
+        public void PaySalary()
+        {
+            Doctor = 90000; 
+            Nurse = 50000;
+            Receptionist = 45000;
+            Janitor = 40000;
+        
+        }
+
+
+    }
+}
